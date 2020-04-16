e1e8a570c3ccd01cd55e94116f59996d1ec79626
ktrutmann <k.trutmann@unibas.ch>
2020-04-17 00:18:30 +0200

    Implemented the fix_channels function

--- mne_pipeline.py
+++ mne_pipeline.py
@@ -1,10 +1,10 @@
 import mne
 
+
 class EEGPrep(object):
     """
     Allows to load EEG data from file, to perform different preprocessing steps
     and pickle the results or export them as pandas DataFrames.
-
     """
 
     def __init__(self, eeg_path, trigger_dict):
@@ -35,11 +35,46 @@ class EEGPrep(object):
         # import data
         self.raw = mne.io.read_raw_edf(self.eeg_path, preload=True)
 
-    def fix_channels(self):  # TODO (Kevin): Implement
+    def fix_channels(self, montage_path, n_ext_channels, ext_ch_mapping=None):
         """
-        EXPLAIN TO MEEE!!!
+        Removes the '1-' from the start of the channel names, sets the Montage (telling MNE which electrode went where)
+        and sets the type of the additional electrodes. For the fixing of the channel names it is assumed that the
+        last channel is the 'Status' channel.
+
+        Parameters
+        ----------
+        montage_path : str
+            A valid string path pointing to the montage folder of the MNE module.
+
+        n_ext_channels : int
+            The number of extra channels used above the 64 standard ones. This includes the reference channels
+            but not the Stim (trigger) channel.
+
+        ext_ch_mapping : dict of {str : str}
+            A dictionary containing the extra channels (including the 'Status' or 'Stim' channel!) as key and their
+            type ('eog', 'emg', 'resp') as values. A standard mapping using seven extra electrodes is provided if this
+            argument is omitted, but it is recommended to explicitly define one. If the standard mapping is used,
+            `n_ext_channels` is overwritten to 9.
         """
-        pass
+
+        if ext_ch_mapping is None:
+            ext_ch_mapping = {'FT10': 'eog', 'PO10': 'eog', 'HeRe': 'eog', 'HeLi': 'emg', 'VeUp': 'emg', 'VeDo': 'emg',
+                            'EMG1a': 'emg', 'Status': 'resp'}  # TODO: discuss
+            n_ext_channels = 9
+
+        # Removing superfluous '1-' in ch_names
+        for name in self.raw.ch_names[:-1]:
+            self.raw.rename_channels({name: name[2:]})
+
+        # Drop unused channels
+        self.raw.drop_channels(self.raw.ch_names[64 + n_ext_channels:len(self.raw.ch_names) - 1])
+
+        # Set montage
+        montage = mne.channels.read_montage(kind='biosemi64', path=montage_path)
+        self.raw.set_montage(montage)
+
+        # Set channel types
+        self.raw.set_channel_types(mapping=ext_ch_mapping)
 
     def set_references(self):  # TODO (Peter): Implement
         """
@@ -69,7 +104,7 @@ class EEGPrep(object):
 
         """
 
-        events = mne.find_events(raw = self.raw, **kwargs)
+        events = mne.find_events(raw=self.raw, **kwargs)
 
         return events
 
