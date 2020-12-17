8daefbf86e3d5247f159cc9ff3f73e836ce2709d
PeterMKraemer <peter.kraemer@unibas.ch>
2020-12-17 17:25:14 +0100

    copied my LRP function and outlined a logic for computing the LRP

--- mne_pipeline_analysis.py
+++ mne_pipeline_analysis.py
@@ -20,18 +20,35 @@ class EEGAnalysis(object):
 
         self.preprocessed = None
         self.epochs = None
+        if prepared_dat_preprocessed_name is not None:
+            if prepared_dat_preprocessed_name.endswith('.bdf'):
+                self.raw = mne.io.read_raw_bdf(prepared_dat_preprocessed_name, preload=True)
+            elif prepared_dat_preprocessed_name.endswith('.fif'):
+                self.raw = mne.io.read_raw_fif(prepared_dat_preprocessed_name, preload=True)
+            else:
+                raise ValueError('Please provide a .bdf or .fif file for the prepared raw data.')
 
-        if prepared_dat_preprocessed_name.endswith('.bdf'):
-            self.raw = mne.io.read_raw_bdf(prepared_dat_preprocessed_name, preload=True)
-        elif prepared_dat_preprocessed_name.endswith('.fif'):
-            self.raw = mne.io.read_raw_fif(prepared_dat_preprocessed_name, preload=True)
-        else:
-            raise ValueError('Please provide a .bdf or .fif file for the prepared raw data.')
-
-        if prepared_dat_epochs_name.endswith('.fif'):
-            self.epochs = mne.read_epochs(prepared_dat_epochs_name, preload=True)
-        else:
-            raise ValueError('Please provide a .bdf or .fif file for the prepared epoched data.')
+        if prepared_dat_epochs_name is not None:
+            if prepared_dat_epochs_name.endswith('.fif'):
+                self.epochs = mne.read_epochs(prepared_dat_epochs_name, preload=True)
+            else:
+                raise ValueError('Please provide a .bdf or .fif file for the prepared epoched data.')
 
     # TODO (All): Create methods to get the LRP plot
-    # TODO (Peter): Copy your code for this in here and go over the code again.
+    def ComputeLRP(self,electrodes=['C3','C4']):
+        """
+        Parameters
+        ----------
+
+        """
+
+        # 1. check if data is epoched.
+
+        # 2. compute average signal for trials with left/right button press
+
+        # 3. subtract the data like in the code snippet below
+
+        #def CalculateLRP(dataLeft, dataRight, channelsLeft, channelsRight):
+        #    output = (dataRight[:, channelsLeft, :, :] - dataLeft[:, channelsLeft, :, :]) - (
+        #                dataRight[:, channelsRight, :, :] - dataLeft[:, channelsRight, :, :])
+        #    return output
--- test_mne_pipeline_analysis.py
+++ test_mne_pipeline_analysis.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import mne_pipeline_prep
 import mne_pipeline_analysis
+import mne_pipeline
 
 
 def test_pipeline_kev_dat():
@@ -12,4 +13,5 @@ def test_pipeline_laura_dat():
     pass
     
 def test_pipeline_peter_dat():
-    pass
+    # load preprocessed data
+    data = mne_pipeline.EEGAnalysis(prepared_dat_preprocessed_name='Data/prepared/participant_xxx_prepared_raw.fif')
